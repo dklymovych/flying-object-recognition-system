8d036e1cf23746771935d87f4fd39a90d6425501
Den <90708440+dklymovych@users.noreply.github.com>
2025-03-14 20:49:20 +0200

    add brightness histogram and contrast enhancement method

--- src/main_window.hpp
+++ src/main_window.hpp
@@ -3,6 +3,7 @@
 #include <QMainWindow>
 #include <QLabel>
 #include <QPixmap>
+#include <opencv2/opencv.hpp>
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -15,7 +16,17 @@ private:
     QPixmap pixmap_;
     QLabel *imageLabel_;
 
+    void createMenuBar();
+    void createAnalysisSection();
+
+    cv::Mat convertQPixmapToMat(const QPixmap &pixmap);
+    QPixmap convertMatToQPixmap(const cv::Mat &mat);
+    void resizeImage(cv::Mat &img);
+
 private slots:
     void loadImage();
     void saveImage();
+
+    void buildBrightnessHistogram();
+    void improveContrast();
 };
